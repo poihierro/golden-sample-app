12d86ebff197e4f8d4e77b653ee8e58516b30a9b
Serhii Koshman <koshman.animys@gmail.com>
2022-03-24 12:11:05 +0200

    add call of the payments service for testing

--- apps/golden-sample-app/src/app/user-accounts/user-accounts-view/user-accounts-view.component.ts
+++ apps/golden-sample-app/src/app/user-accounts/user-accounts-view/user-accounts-view.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaymentsService } from '../../services/payments.service';
 import { UserAccountsService } from '../user-accounts.service';
 
 @Component({
   selector: 'app-user-accounts-view',
   templateUrl: './user-accounts-view.component.html',
 })
-export class UserAccountsViewComponent {
+export class UserAccountsViewComponent implements OnInit {
   public arrangements$ = this.userAccountsService.arrangements$;
 
-  constructor(private readonly userAccountsService: UserAccountsService) {}
+  constructor(
+    private readonly userAccountsService: UserAccountsService,
+    private readonly payments: PaymentsService,
+  ) {}
+
+  ngOnInit(): void {
+    this.payments.checkValidatePayment(this.payments.paymentForValidation).subscribe(v => console.log(v));
+      
+  }
 }
