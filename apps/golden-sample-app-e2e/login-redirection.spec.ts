9e4fb2ca16acaab59c59e91ef7dd3744ed513bd2
Florian PASTEUR <florian.c.p@pm.me>
2023-02-22 10:14:48 +0100

    create a login test

--- apps/golden-sample-app-e2e/login-redirection.spec.ts
+++ apps/golden-sample-app-e2e/login-redirection.spec.ts
@@ -1,6 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
+test.use({
+  storageState: {
+    cookies: [],
+    origins: []
+  }
+})
+
+test('should redirect to the login page', async ({ page }) => {
   await page.goto('');
   const title = page.locator('#kc-page-title');
   await expect(title).toHaveText('Welcome to your online banking');
