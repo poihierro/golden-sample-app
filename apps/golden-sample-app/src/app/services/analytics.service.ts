bdbf6178255a541887b7a8eb42b20619eb0d8b53
ankypant <ankit12.pant@gmail.com>
2023-07-04 15:09:44 +0200

    chore: add GA example

--- apps/golden-sample-app/src/app/services/analytics.service.ts
+++ apps/golden-sample-app/src/app/services/analytics.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { TrackerHandler } from '@backbase/foundation-ang/observability';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+declare const gtag: any;
+
 /* 
 This service will receive all the analytics events from your application and from here you can
 send all your tracker events to the analytics system (eg: google analytics/segment stc)
@@ -10,6 +13,12 @@ export class AnalyticsService extends TrackerHandler {
   register(): void {
     this.tracker.subscribeAll((event) => {
       console.log('EVENT TRACKER', event);
+
+      gtag('event', event.name, {
+        event_category: event.type,
+        event_label: event.name,
+        value: JSON.stringify(event.payload),
+      });
     });
 
     // this.tracker.subscribe(ScreenResizeEvent, event => {
