9f6c3dbb9e12b26d5af81115d1caf0387425a606
ankypant <ankit12.pant@gmail.com>
2023-08-09 14:43:26 +0200

    chore: fix otel endpoint in mock config

--- apps/golden-sample-app/src/environments/environment.mocks.ts
+++ apps/golden-sample-app/src/environments/environment.mocks.ts
@@ -23,7 +23,7 @@ export const environment: Environment = {
   mockEnabled: true,
   isTracerEnabled: true,
   bbApiKey: '27d4d4ee-afc1-4190-adc4-b9d30d39badb',
-  otelURL: 'https://botel.bartbase.com/v1/trace',
+  otelURL: 'https://botel.bartbase.com/v1/traces',
 };
 
 export const authConfig: AuthConfig = {
